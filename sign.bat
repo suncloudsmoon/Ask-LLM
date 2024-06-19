9b51162dd283391443d59013fe6e21b645188905
suncloudsmoon <34616349+suncloudsmoon@users.noreply.github.com>
2024-06-18 19:35:16 -0700

    Delete sign.bat

--- sign.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /s Root /sha1 7E6A07CC1ED47E09DD939E9E169396F9A5AF26EA /fd SHA256 "bin\x64\Release\net6.0-windows\publish\Ask-LLM-AddIn64-packed.xll"
